1eaa383e20e52651153c4a3c03ecef95f7ae9182
zanluthfi <fauxanluthfi@gmail.com>
2025-02-12 11:52:08 +0700

    first commit

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+$nama = "ojan";
+$email = "email@gmail.com";
+$pekerjaan = "jobless";
+$universitas = "unpas";
+
+echo "<h1>$nama</h1>";
+echo "<h4>$email</h4>";
+echo "<p>saya sebagai $pekerjaan, dan berkuliah di $universitas</p>";
+?>
