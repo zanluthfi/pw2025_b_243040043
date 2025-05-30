ca07fae76924c4938e87c9ebde6caf19abab8f0f
zanluthfi <fauxanluthfi@gmail.com>
2025-05-30 20:51:56 +0700

    y

---
+++ kuliah/pertemuan10/index.php
@@ -0,0 +1,47 @@
+<?php
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h1>Daftar Mahasiswa</h1>
+  <a href="tambah.php">Tambah data mahasiswa</a>
+  <br><br>
+  <table border="1" cellspacing="0" cellpadding="10">
+    <tr>
+      <th>No.</th>
+      <th>Nama</th>
+      <th>NIM</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Gambar</th>
+      <th>Aksi</th>
+    </tr>
+    <?php $i = 1;
+    foreach ($mahasiswa as $row) : ?>
+      <tr>
+        <td><?= $i++ ?></td>
+        <td><?= $row['nama'] ?> </td>
+        <td><?= $row['nrp'] ?></td>
+        <td><?= $row['email'] ?></td>
+        <td><?= $row['jurusan'] ?></td>
+        <td>
+          <img src="img/<?= $row['gambar'] ?>" width="50">
+        </td>
+        <td>
+          <a href="">ubah</a> | <a href="">hapus</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
---
+++ kuliah/pertemuan10/tambah.php
@@ -0,0 +1,47 @@
+<?php
+require 'functions.php';
+
+if ( isset ( $_POST[ 'submit' ] ) ) {
+
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah data mahasiswa</title>
+</head>
+<body>
+    <h1>Tambah data mahasiswa</h1>
+
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="nrp">NRP :</label>
+                <input type="text" name="nrp" id="nrp">
+            </li>
+            <li>
+                <label for="nama">Nama :</label>
+                <input type="text" name="nama" id="nama">
+            </li>
+            <li>
+                <label for="email">Email :</label>
+                <input type="text" name="email" id="email">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan :</label>
+                <input type="text" name="jurusan" id="jurusan">
+            </li>
+            <li>
+                <label for="gambar">Gambar :</label>
+                <input type="text" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Tambah Data!</button>
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
